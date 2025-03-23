438988f6071033b9ebf6a14fd5b9ece7d6de25c3
subratom <subratom@users.noreply.github.com>
2025-03-22 23:39:03 -0700

    Removed unnecessary declarations from the code

--- src/routes/v1/caseRoutes.ts
+++ src/routes/v1/caseRoutes.ts
@@ -1,8 +1,6 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { fetchCaseStatus } from '../../controllers/v1/uscisCase.controller';
 import { fetchCaseStatusHistory } from '../../controllers/v1/uscisCaseHistory.controller';
-import axios from 'axios';
-import * as cheerio from 'cheerio';
 
 //import CaseMongo from '../models/CaseMongo';
 //import { getPostgresDataSource } from '../db/postgres';
